abfa44b45f17da392bd57243dd3a8bc8bb6036a0
Jay Dharamshi <jay.7995@gmail.com>
2022-07-01 17:41:05 +0530

    Hardhat basic setup

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+module.exports = {
+    // solidity: "0.8.8",
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+}
